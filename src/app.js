ee838aa8d8dbc2e22eddc8ad9baa1a294337b9c9
bibliomaniac3 <psalm.4v3@gmail.com>
2020-02-06 20:53:45 -0500

    added css loader
    Co-authored-by: EdricY <yu.edric@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,3 @@
+require('../styles/index.css');
+
+require('./main.js');
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,24 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: {
-    app: './src/main.js',
+    app: './src/app.js',
   },
   plugins: [
     // new CleanWebpackPlugin(),
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/src'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 };
